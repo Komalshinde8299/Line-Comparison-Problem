f1ec14d2e8316400782ca28b21bae2de545ef30d
Komal Shinde <komalshinde8299@gmail.com>
2023-05-27 18:12:12 +0530

    Added:Welcome msg for line comparison problem

---
+++ src/com/first/line_comparision_problem/line_Comparison_Problem.java
@@ -0,0 +1,7 @@
+package com.first.line_comparision_problem;
+
+public class line_Comparison_Problem {
+    public static void main(String[] args) {
+        System.out.println("* Wellcome to line comparison problem *");
+    }
+}
