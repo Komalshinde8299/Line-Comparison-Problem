a282ef0222569b0decb3c86e6648e52542a04d08
Komal Shinde <komalshinde8299@gmail.com>
2023-05-27 18:22:20 +0530

    Added:Code to get length of line

--- src/com/first/line_comparision_problem/line_Comparison_Problem.java
+++ src/com/first/line_comparision_problem/line_Comparison_Problem.java
@@ -2,6 +2,13 @@ package com.first.line_comparision_problem;
 
 public class line_Comparison_Problem {
     public static void main(String[] args) {
-        System.out.println("* Wellcome to line comparison problem *");
+        System.out.println("* Welcome to line comparison problem *");
+        double x1 = 1;
+        double y1 = 2;
+        double x2 = 5;
+        double y2 = 7;
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("Length of the line: " + length);
     }
 }
+
